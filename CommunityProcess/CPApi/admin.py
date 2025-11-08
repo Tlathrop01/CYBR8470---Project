102b1457103cb963310516b5673b10c402156f49
Tlathrop01 <tlathrop@unomaha.edu>
2025-11-07 19:21:01 -0600

    added first model and registered on the admin file

--- CommunityProcess/CPApi/admin.py
+++ CommunityProcess/CPApi/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Issue
 
 # Register your models here.
+
+
+class IssueAdmin(admin.ModelAdmin):
+    # Define the list of fields to display in the admin interface
+    list_display = ('Issue_name')
+    
+    # Add search functionality for specific fields
+    search_fields = ('Issue_name', 'description')
+
+    # Add filters for the age and breed fields in the sidebar
+    list_filter = ('Issue_name', 'description')
+
+    # Define which fields can be clicked to view the details page
+    list_display_links = ('Issue_name',)
+
+    # Define how fields are displayed when editing a Dog instance
+    fields = ('Issue_name', 'description')
+
+# Register the model and admin class
+admin.site.register(Issue, IssueAdmin)
--- CommunityProcess/CPApi/models.py
+++ CommunityProcess/CPApi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Issue(models.Model):
+    Issue_name = models.CharField(max_length=25)
+    description = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
