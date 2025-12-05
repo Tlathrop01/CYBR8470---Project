db3b0472275693b42061332a550865c2919516c1
Tlathrop01 <tlathrop@unomaha.edu>
2025-12-04 19:06:28 -0600

    Adding css style and images to fill out the website

--- CommunityProcess/CPApi/tests.py
+++ CommunityProcess/CPApi/tests.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
-
+from django.urls import reverse
 from .models import Question
 
 
@@ -13,15 +13,65 @@ class QuestionModelTests(TestCase):
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
-def test_was_published_recently_with_old_question(self):
-    #test for old questions
-    time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-    old_question = Question(pub_date=time)
-    self.assertIs(old_question.was_published_recently(), False)
+    def test_was_published_recently_with_old_question(self):
+        #test for old questions
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
 
 
-def test_was_published_recently_with_recent_question(self):
-    #test for new questions
-    time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-    recent_question = Question(pub_date=time)
-    self.assertIs(recent_question.was_published_recently(), True)
+    def test_was_published_recently_with_recent_question(self):
+        #test for new questions
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+def create_question(question_text, days):
+    #Creates an issue for testing purposes
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+#Tests the question created
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        #Tests if no issues present  and verifies the list of issues
+        response = self.client.get(reverse("CPApi:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No issues are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_past_question(self):
+        #Issues with a past pub_date are displayed
+        question = create_question(question_text="Past issue.", days=-30)
+        response = self.client.get(reverse("CPApi:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question],
+        )
+
+    def test_future_question(self):
+        #Issues with a future pub_date are displayed
+        create_question(question_text="Future issue.", days=30)
+        response = self.client.get(reverse("CPApi:index"))
+        self.assertContains(response, "No issues are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_future_question_and_past_question(self):
+        #Displays past issues
+        question = create_question(question_text="Past issue.", days=-30)
+        create_question(question_text="Future issue.", days=30)
+        response = self.client.get(reverse("CPApi:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question],
+        )
+
+    def test_two_past_questions(self):
+        #Allows the display of multiple issues
+        question1 = create_question(question_text="Past issue 1.", days=-30)
+        question2 = create_question(question_text="Past issue 2.", days=-5)
+        response = self.client.get(reverse("CPApi:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question2, question1],
+        )
--- CommunityProcess/CPApi/views.py
+++ CommunityProcess/CPApi/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-
+from django.utils import timezone
 from .models import Choice, Question
 
 
@@ -12,8 +12,10 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        #Returns the five most recently published issues
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+        :5
+    ]
 
 
 class DetailView(generic.DetailView):
